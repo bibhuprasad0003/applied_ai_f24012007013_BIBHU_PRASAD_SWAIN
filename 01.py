af68f903696daec642ca5ade8a2c7ef594eb76e5
BIBHU PRASAD SWAIN <bibhuprasada0003@gmail.com>
2026-06-01 12:36:35 +0530

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,24 @@
+#integer type
+a = 10
+print(a)
+print(type(a))
+#float type 
+b = 3.14
+print(b)
+print(type(b))
+#character type
+e = 'A'
+print(e)
+print(type(e))
+#string type
+c = "Hello, World!"
+print(c)
+print(type(c))
+#boolean type
+d = True
+print(d)
+print(type(d))      
+a="10"
+b=int(a)
+print(b)
+print(type(b))
---
+++ sumoftennumbers.py
@@ -0,0 +1,4 @@
+a=int(input("Enter ten number: "))
+for i in range(1,11):
+    a=a+i
+print(a)
